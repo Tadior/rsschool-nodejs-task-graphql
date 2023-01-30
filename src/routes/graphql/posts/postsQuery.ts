ea7680497c9c01ae20642d6e7d39b7fbb050a65a
Dmitriy Zamulin <zamulin1998@gmail.com>
2023-01-30 18:28:03 +0300

    refactor: make tests green, fix var name

--- src/routes/graphql/posts/postsQuery.ts
+++ src/routes/graphql/posts/postsQuery.ts
@@ -1,11 +1,9 @@
 import { GraphQLList } from 'graphql';
-import { postQueryType } from './PostQueryType';
+import { postQueryType } from './postQueryType';
 
-const postsQuery = {
+export const postsQuery = {
   type: new GraphQLList(postQueryType),
   resolve: async (_: any, args: any, fastify: any) => {
     return await fastify.db.posts.findMany();
   },
 };
-
-export { postsQuery };
